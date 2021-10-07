ac7bba347434b3740c206c33ae4d8f8ad74acd4d
tanmayshukla82 <67494383+tanmayshukla82@users.noreply.github.com>
2021-10-08 00:25:32 +0530

    =update-delete-student

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const connection = require('./app/config/connection');
 const student_register = require('./routes/stud_register');
+const getStudents = require('./routes/stud_read');
+const updateStudent = require('./routes/stud_update');
+const deleteStudent = require('./routes/stud_delete');
 connection();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(student_register);
+app.use(getStudents);
+app.use(updateStudent);
+app.use(deleteStudent);
 
 app.get('/',(req,res)=>{
     res.send("Hello");
 })
+
 
 app.listen(PORT,()=>{
     console.log("connection successful!!");
