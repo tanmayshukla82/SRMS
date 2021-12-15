2082466d713a16bd1055c5f8e411cd5b86411b5a
tanmayshukla82 <67494383+tanmayshukla82@users.noreply.github.com>
2021-12-15 19:29:36 +0530

    update

--- app/config/connection.js
+++ app/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 function connectDB(){
 const url = process.env.MONGO_URL;
-mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology: false});
+mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology: true});
 const db = mongoose.connection;
 db.once('open',()=>{
     console.log("Database connected!!");
