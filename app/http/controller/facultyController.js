ac652aa450e7818a684c0e3b0832f20f941d492b
tanmayshukla82 <67494383+tanmayshukla82@users.noreply.github.com>
2021-12-19 10:34:20 +0530

    update

--- app/http/controller/facultyController.js
+++ app/http/controller/facultyController.js
@@ -158,6 +158,24 @@ function facultyController(){
                 return res.status(400).render('./faculty/updateMarksFaculty',{layout : './layouts/facultyDashboard.ejs'})
             }
         },
+         postUpdateMarksFaculty : async(req,res)=>{
+            try {
+                const {registrationNumber, subjectCode, subjectName, mark,examType} = req.body;
+                const stud = await Student.find({registrationNumber});
+                for(let i=0;i<subjectCode.length;i++)
+                {
+                    const s = await Subject.findOne({subjectCode:subjectCode[i]});
+                    const m = await Marks.findOne({student:stud[0],subject:s._id});
+                    m.marks = mark[i];
+                    await m.save();
+                }
+                req.flash('success','Marks updated successfully');
+                return res.status(200).render('./admin/updateMarks.ejs',{layout : './layouts/adminDashboard.ejs'});
+            } catch (error) {
+                req.flash('error','Error in updating marks');
+                return res.status(400).render('./admin/postUpdateMarksPage.ejs',{layout : './layouts/adminDashboard.ejs'});
+            }
+        },
         updatePassword : async(req, res)=>{
             try {
                 const {oldPassword, newPassword, confirmPassword, email} = req.body;
